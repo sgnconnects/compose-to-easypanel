409b10a1807b31476f7fb12905b65b89f42da158
ravenbroetzmann <ravenbroetzmann@mailbox.org>
2022-08-07 14:06:58 +0200

    parser schape

---
+++ packages/parser/index.ts
@@ -0,0 +1,3 @@
+import { Parser } from "./parser";
+const parser = new Parser();
+export default parser;
---
+++ packages/parser/parser.ts
@@ -0,0 +1,19 @@
+export class Parser {
+  private _yml: object = {};
+  private _schema: object = {};
+
+  private parseYmlToSchema() {}
+
+  public set setYml(ymlString: string) {
+    /**
+     * @TODO parse Yml string
+     */
+  }
+
+  public get getSchema(): object {
+    /**
+     * @TODO parse Schema
+     */
+    return this._schema;
+  }
+}
