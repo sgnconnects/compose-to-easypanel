95628dfb3c06acc5132e933e48e301db0e669248
ravenbroetzmann <ravenbroetzmann@mailbox.org>
2022-08-08 11:44:07 +0200

    serviceParser test

--- tests/easypanel.serviceParser.test.ts
+++ tests/easypanel.serviceParser.test.ts
@@ -1,6 +1,10 @@
 import {
+  parseAppService,
+  parseMariaDbService,
+  parseMongoService,
   parseMySqlService,
   parsePostgresService,
+  parseRedisService,
 } from "../packages/parser/easypanel/serviceParser";
 
 describe("test the service Parsers", () => {
@@ -42,8 +46,95 @@ describe("test the service Parsers", () => {
       },
     });
   });
-  it("Maria Service", () => {});
-  it("Mongo Service", () => {});
-  it("Redis Service", () => {});
-  it("App Service", () => {});
+  it("Maria Service", () => {
+    expect(
+      parseMariaDbService(
+        "project",
+        "mariadb",
+        { MARIADB_ROOT_PASSWORD: "pw_root", MARIADB_PASSWORD: "pw_normal" },
+        "mariadb"
+      )
+    ).toEqual({
+      type: "mariadb",
+      data: {
+        projectName: "project",
+        serviceName: "mariadb",
+        image: undefined,
+        password: "pw_normal",
+        rootPassword: "pw_root",
+      },
+    });
+  });
+  it("Mongo Service", () => {
+    expect(
+      parseMongoService(
+        "project",
+        "mongo",
+        { MONGO_INITDB_ROOT_PASSWORD: "pw" },
+        "mongo"
+      )
+    ).toEqual({
+      type: "mongo",
+      data: {
+        projectName: "project",
+        serviceName: "mongo",
+        image: undefined,
+        password: "pw",
+      },
+    });
+  });
+  it("Redis Service", () => {
+    expect(
+      parseRedisService("project", "redis", { REDIS_PASSWORD: "pw" }, "redis")
+    ).toEqual({
+      type: "redis",
+      data: {
+        projectName: "project",
+        serviceName: "redis",
+        image: undefined,
+        password: "pw",
+      },
+    });
+  });
+  it("App Service", () => {
+    expect(
+      parseAppService(
+        "project",
+        "app",
+        "docker",
+        ["5000:8000"],
+        { API_KEY: "apiKey" },
+        ["./bind:/app/bind", "volume:/app/volume"],
+        ["yarn", "start"]
+      )
+    ).toEqual({
+      type: "app",
+      data: {
+        projectName: "project",
+        serviceName: "app",
+        source: {
+          type: "image",
+          image: "docker",
+        },
+        ports: [
+          {
+            published: 5000,
+            target: 8000,
+          },
+        ],
+        env: "API_KEY=apiKey",
+        mounts: [
+          {
+            type: "bind",
+            hostPath: "./bind",
+            mountPath: "/app/bind",
+          },
+          { type: "volume", name: "volume", mountPath: "/app/volume" },
+        ],
+        deploy: {
+          command: "yarn start",
+        },
+      },
+    });
+  });
 });
